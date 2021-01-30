573f9d299307ff6e2c81c1b67c31c875075637cd
kadir <kadir.bekr@gmail.com>
2021-01-30 20:08:35 +0300

    Change text view value by clicking button example added.

--- kotlin_series/app/src/main/java/com/example/learningkotlinseries/MainActivity.kt
+++ kotlin_series/app/src/main/java/com/example/learningkotlinseries/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.learningkotlinseries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var showMessageButton:Button
+    private lateinit var messageTextView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        messageTextView = findViewById(R.id.messageTextView)
+        showMessageButton = findViewById(R.id.showMessageButton)
+
+        showMessageButton.setOnClickListener {
+            messageTextView.text = "Hello World!"
+        }
     }
 }
